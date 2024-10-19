5284b3845687fb95c497a57cb0fd23853e8a9aeb
Paweł Łaba <labapawel@gmail.com>
2024-10-19 15:11:22 +0200

    generator numerów pesel

--- app2/Pesel.cs
+++ app2/Pesel.cs
@@ -1,5 +1,8 @@
 
+using System.Runtime.CompilerServices;
+
 namespace app1 {
+public enum Plec {M, K};
 public class Pesel {
     private string _pesel = "";
     public string pesel {
@@ -13,6 +16,71 @@ public class Pesel {
             return (int.Parse(_pesel[9].ToString()) % 2 == 0) ? "Kobieta" : "Facet";
         }
     }
+
+    public string generator(DateTime? data = null, Plec plec = Plec.K, int? num = null)
+    {
+        if(data == null){
+            data = DateTime.Now;
+        }
+        if(num == null)
+        {
+            Random rnd = new Random();
+            num = rnd.Next(0,9999);
+            if(plec == Plec.K && ((num % 2)== 1) ) num++;
+            else 
+            if(plec == Plec.M && ((num % 2)== 0) ) num++; 
+        }
+        int r = data.Value.Year;
+        int m = data.Value.Month;
+        int d = data.Value.Day;
+        if(r >= 2200) { m+=60; r-=2200; } else
+        if(r >= 2100) { m+=40; r-=2100; } else
+        if(r >= 2000) { m+=20; r-=2000; } else
+        if(r >= 1900) { r -= 1900; }
+        else { m+=80; r -= 1800; }
+
+        string pesel = r.ToString("D2") + m.ToString("D2") + d.ToString("D2") + num.Value.ToString("D4");
+
+        int[] wagi = {9,7,3,1,9,7,3,1,9,7};
+        int suma =0;
+        for(int i=0; i<10; i++)
+            suma += int.Parse(pesel[i].ToString()) * wagi[i];
+
+        pesel += suma % 10;
+
+
+        return pesel;
+    }
+
+    public DateTime? dataur {
+        get {
+            if(!valid) return null;
+            int rok = int.Parse(_pesel.Substring(0,2));
+            int mies = int.Parse(_pesel.Substring(2,2));
+            int dzien = int.Parse(_pesel.Substring(4,2));
+
+            if(mies>80){
+                rok += 1800;
+                mies -= 80;
+            } else
+            if(mies>60){
+                rok += 2200;
+                mies -= 60;
+            } else
+            if(mies>40){
+                rok += 2100;
+                mies -= 40;
+            } else
+            if(mies>20){
+                rok += 2000;
+                mies -= 20;
+            } else rok += 1900;
+
+
+          return new DateTime(rok, mies, dzien);
+        }
+    }
+
     private int sumaKontrolna = 0;
     private bool valid = false;
     private void WpiszPesel(string val){
@@ -29,7 +97,7 @@ public class Pesel {
         if(valid)
             _pesel = val;
     }
-    private string czytajPesel(){
+    private string czytajPesel(){ 
         return this._pesel;
     }
 
--- app2/Program.cs
+++ app2/Program.cs
@@ -1,5 +1,14 @@
 ﻿using app1;
 
+/**
+    mies 80+m rok = 1800-1899
+    mies 60+m rok = 2200-2299
+    mies 40+m rok = 2100-2199
+    mies 20+m rok = 2000-2099
+    mies 0+m rok = 1900-1999
+*/
+
+
 Klasa3 k = new Klasa3();
 k.Klasa.inne = 20;
 
@@ -9,7 +18,8 @@ Console.WriteLine(k.Klasa.inne);
 Console.WriteLine(k.dane);
 
 Pesel p = new Pesel();
-p.pesel = "22222222222";
+p.pesel = "22810128916"; 
 
 
-Console.WriteLine(" pesel: {0} płeć: {1}",  p.pesel, p.plec);
+
+Console.WriteLine(" pesel: {0} płeć: {1} Data ur {2:yyyy-MM-dd} {3}",  p.pesel, p.plec, p.dataur, p.generator(new DateTime(1822,1,1),Plec.M));
